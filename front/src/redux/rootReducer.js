c7eeb163d0b5c9b57b8c782f922ae73e2d11e289
SyotaKawakami <49909868+syotaKawakami@users.noreply.github.com>
2020-04-06 06:50:53 +0900

    addScheduleDialog reducer&actions

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import calendarReducer from "./calendar/reducer";
+import addScheduleReducer from "./addSchedule/reducer";
 
 // reducerを一箇所で管理・まとめる
-const rootReducer = combineReducers({ calendar: calendarReducer });
+const rootReducer = combineReducers({
+  calendar: calendarReducer,
+  addSchedule: addScheduleReducer
+});
 
 
 export default rootReducer;
